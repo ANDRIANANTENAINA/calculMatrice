66bf6224b9ec794c3287f3c5b709bae09adea9ac
i-Falinirina <antnfaly@gmail.com>
2021-03-11 09:57:25 +0100

    final

--- src/tpjava/TpJava.java
+++ src/tpjava/TpJava.java
@@ -5,6 +5,9 @@
  */
 package tpjava;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,16 +21,17 @@ public class TpJava {
      * @param taileLine
      * @param taileCol
      * @return
+     * @throws java.security.NoSuchAlgorithmException
      */
-    public static long[][] generateMatrice(int taileLine, int taileCol) {
+    public static long[][] generateMatrice(int taileLine, int taileCol) throws NoSuchAlgorithmException {
+        Random rand = SecureRandom.getInstanceStrong();
         long[][] matrice = new long[taileLine][taileCol];
         for (int i = 0; i < taileLine; i++) {
+            for (int j = 0; j < taileCol; j++) {
+                matrice[i][j] = rand.nextInt(100);
 
+            }
         }
-        for (int i = 0; i < taileCol; i++) {
-
-        }
-
         return matrice;
     }
 
@@ -41,43 +45,47 @@ public class TpJava {
 
     public static Matrice calculProduitThread(Matrice m1, Matrice m2) {
         Matrice p = new Matrice(m1.getLine(), m2.getCol());
-
-        ThreadMatrice thread;
+        ThreadMatrice thread[] = new ThreadMatrice[m1.getCol()];
+//      lance les threads
         for (int i = 0; i < m1.getCol(); i++) {
-            thread = new ThreadMatrice(m1.getLine(i), m2.getM());
-            thread.start();
+            thread[i] = new ThreadMatrice(m1.getLine(i), m2.getM());
+            thread[i].start();
+        }
+//      recuperation 
+        for (int i = 0; i < m1.getCol(); i++) {
             try {
-                thread.join();
+                thread[i].join();
             } catch (InterruptedException ex) {
                 Logger.getLogger(TpJava.class.getName()).log(Level.SEVERE, null, ex);
             }
-            p.setMatrice(thread.getLineResult(), i);
+            p.setMatrice(thread[i].getLineResult(), i);
 
         }
 
         return p;
     }
 
-    public static void main(String[] args) {
-        long[][] a = {{1, 2}, {3, 4}};
-        long[][] b = {{5, 6}, {7, 8}};
-        long[][] c = {{1, 2, 3}, {4, 5, 6}, {4, 5, 6}};
-        long[][] d = {{1, 2}, {3, 4}, {5, 6}};
-        long[][] e = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+    public static void main(String[] args) throws NoSuchAlgorithmException {
 
         Matrice m1, m2, resultat, resultat2;
-        m1 = new Matrice(c);
-        m2 = new Matrice(d);
+        m1 = new Matrice(generateMatrice(2000, 2000));
+        m2 = new Matrice(generateMatrice(2000, 2000));
 
 //      Temps d'execution sans affichage
         long begin = System.currentTimeMillis();
-        resultat = calculProduitThread(m1, m2);
-//        resultat2 = calculProduitSimple(m1, m2);
+//        resultat = calculProduitThread(m1, m2);
+        resultat = calculProduitSimple(m1, m2);
         long finish = System.currentTimeMillis();
+        
+        long begin2 = System.currentTimeMillis();
+        resultat2 = calculProduitThread(m1, m2); // nombre de colnne threads m2 lancee
+//        resultat = calculProduitSimple(m1, m2);
+        long finish2 = System.currentTimeMillis();
 
-        System.out.println(resultat.affiche());
+//        System.out.println(resultat.affiche());
 //        System.out.println(resultat2.affiche());
-        System.out.println("Durrée = " + (finish - begin) + " ms");
-    }
+        System.out.println("Durrée simple = " + (finish - begin) + " ms");
+        System.out.println("Durrée thread = " + (finish2 - begin2) + " ms");
 
+    }
 }
